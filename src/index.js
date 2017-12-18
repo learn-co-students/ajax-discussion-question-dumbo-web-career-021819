e9efd1a3e9aaa361b657e137111221a38e90cf9b
github username <andrewcohn0@gmail.com>
2017-12-18 11:02:29 -0500

    change script tag for index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const fullname = document.getElementById("fullname");
+console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("CONTENT LOADED!");
+});
